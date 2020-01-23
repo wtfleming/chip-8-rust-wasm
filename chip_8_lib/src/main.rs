d3871e4b796b01f1b24ec7121123a93f9149608f
Will Fleming <wfleming77@gmail.com>
2020-01-23 15:26:11 -0500

    remove duplicate roms

--- chip_8_lib/src/main.rs
+++ chip_8_lib/src/main.rs
@@ -1,4 +1,3 @@
-
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
@@ -11,10 +10,8 @@ fn main() {
     let mut cpu = Cpu::new();
     cpu.initialize();
 
-    // let data = load_game("../c8games/PONG2").unwrap();
-    let data = load_game("./c8games/PONG2").unwrap();
+    let data = load_game("./chip_8_wasm/static/roms/PONG2").unwrap();
     cpu.load_game(data);
-    //    cpu.load_game("c8games/PONG2");
 
     loop {
         // Emulate one cycle
@@ -27,7 +24,6 @@ fn main() {
 
         // Store key press state (Press and Release)
         // cpu.setKeys();
-
 
         //break;
     }
@@ -49,38 +45,9 @@ fn load_game(file_name: &str) -> io::Result<Vec<u8>> {
     let mut buffer: Vec<u8> = vec![0; file_metadata.len() as usize];
     f.read(&mut buffer)?;
 
-
     // for (i, item) in buffer.iter().enumerate() {
     //     println!("{} {}", i, item);
     // }
 
     Ok(buffer)
 }
-
-
-// fn load_game(file_name: &str) -> io::Result<()> {
-//     println!("load_game() {}", file_name);
-
-//     let file_metadata = std::fs::metadata(file_name)?;
-//     println!("{} is {} bytes in size", file_name, file_metadata.len());
-//     // TODO ensure file size is less than 4096 - 512?
-//     // Since most programs written for the original system begin at memory location 512 (0x200)
-
-//     let mut f = File::open(file_name)?;
-
-//     //    let mut buffer = [0; 4096];
-
-//     let mut buffer = vec![0; file_metadata.len() as usize];
-//     f.read(&mut buffer)?;
-
-
-//     for (i, item) in buffer.iter().enumerate() {
-//         println!("{} {}", i, item);
-//     }
-//     Ok(())
-// }
-
-// 34  = 0x22
-// 246 = 0xF6
-// 107 = 0x6B
-// 12  = 0xC
